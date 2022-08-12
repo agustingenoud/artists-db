59926d9138e5ebbcf8077dde8102b17af13c648c
agustingenoud <agustingenoud@gmail.com>
2022-08-12 09:43:02 -0300

    sliders-txt-update

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import { React, useContext } from "react";
+
 import Carousel from "react-material-ui-carousel";
+import AuthContext from "../Context/AuthContext";
+
 import { Typography, Button, Grid } from "@mui/material";
 
 function Slider(props) {
+  const context = useContext(AuthContext);
+  console.log(">>>>>>>>>> context");
+  console.log(context.color);
+
   var items = [
     {
       name: "Disidencias Sexuales",
@@ -10,24 +17,31 @@ function Slider(props) {
         "Prácticas artísticas y activistas que cuestionan la forma hegemónica en que sexo y genero son representados socialmente",
       img:
         "https://firebasestorage.googleapis.com/v0/b/devenir-otros-cuerpos.appspot.com/o/images%2FDS.jpg?alt=media&token=4bbda1eb-12f0-4e3e-addc-9d10e37828d4",
+      color: context.color.DS,
     },
     {
       name: "Feminismos",
-      description: "FE",
+      description:
+        "Prácticas artísticas desarrolladas por artistas mujeres o disidencias que buscan cuestionar y subvertir la forma en que el poder se distribuye.",
       img:
         "https://firebasestorage.googleapis.com/v0/b/devenir-otros-cuerpos.appspot.com/o/images%2FFE.jpg?alt=media&token=8d7c8ab5-64df-41e7-a007-7177cc140d19",
+      color: context.color.FE,
     },
     {
       name: "Escenas Subterráneas",
-      description: "ES",
+      description:
+        "Entornos de confluencia de músicos, artistas, poetas, editores y afines que desarrollaron sus prácticas en los márgenes de la cultura institucional o hegemónica.",
       img:
         "https://firebasestorage.googleapis.com/v0/b/devenir-otros-cuerpos.appspot.com/o/images%2FES.jpg?alt=media&token=22bea0cf-fbd8-4930-84f5-b928b14ed979",
+      color: context.color.ES,
     },
     {
       name: "Acción Gráfica",
-      description: "AG",
+      description:
+        "Prácticas que proponen intervenciones en espacios públicos a través del uso de herramientas gráficas de alcance popular.",
       img:
         "https://firebasestorage.googleapis.com/v0/b/devenir-otros-cuerpos.appspot.com/o/images%2FAG.jpg?alt=media&token=3a4a61ef-1f38-4358-88da-c0aa339d043e",
+      color: context.color.AG,
     },
   ];
 
@@ -63,8 +77,9 @@ function Item(props) {
         <Typography variant='h5'>
           <span
             style={{
-              backgroundColor: "#D6711E",
+              backgroundColor: props.item.color,
               padding: "0.4vh 1.2vw",
+              color: "white",
             }}
           >
             {props.item.name}
